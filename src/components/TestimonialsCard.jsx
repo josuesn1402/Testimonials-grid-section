b0dffc70762fc81de4892ed5189a8129b9b43e83
Josue Sanchez Nima <josue.sanchez.n1402@hotmail.com>
2022-12-20 18:22:58 -0500

    Import repaired images

--- src/components/TestimonialsCard.jsx
+++ src/components/TestimonialsCard.jsx
@@ -1,22 +1,19 @@
+import { useState, useEffect } from 'react';
+
 function TestimonialsCard(props) {
-  async function importImage(imagePath) {
-    try {
-      const image = await import(imagePath);
-      console.log(image);
-      return image;
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  
-  const image = importImage(props.userAvatar);
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    /* @vite-ignore */
+    import(props.userAvatar).then(img => {
+      setImage(img.default);
+    });
+  }, []);
 
   return (
     <>
       <div className='User'>
-        {
-          <img src={image.default} alt={`Avatar's ${props.userName}`} />
-        }
+        <img src={image} alt={`Avatar's ${props.userName}`} />
         <div className='UserInfo'>
           <h4 className='UserName'>{props.userName}</h4>
           <span>Verified Graduate</span>
